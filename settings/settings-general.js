02b50b887218caa617b7e227209de98740e82eef
csabourin <christian.sabourin@canada.ca>
2020-09-30 14:37:28 -0400

    First commit

---
+++ settings/settings-general.js
@@ -0,0 +1,12 @@
+'use strict';
+define({
+  "courseTitle_en": "",
+  "courseTitle_fr": "Learn-O-Matic",
+  "courseSubtitle_en": "",
+  "courseSubtitle_fr": "Outils SNAP!",
+  "seriesTitle_en": "<div class='first'>Indigenous considerations</div><div class='second'>in federal procurement</div>",
+  "seriesTitle_fr": "<div class='first'>Indigenous considerations</div><div class='second'>in federal procurement</div>",
+  "addToolboxPage": [],
+  showFavorites: false,
+  "editMode": false
+});
